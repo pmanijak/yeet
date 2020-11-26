271f153ce21f5058588c5ed6c7a7fa2195c48128
Philip Manijak <github@morningshift.org>
2020-11-26 12:06:43 -0800

    feat: Send yeet to Slack

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
+require('dotenv').config();
+
 const bodyParser = require('body-parser')
 const app = require('express')()
+const axios = require('axios');
+
+var slackUrl = process.env.SLACK_YEET_URL;
 
 app.use(bodyParser.json())
 app.post('/letters', (req, res) => {
     var data = req.body;
-    console.log(data.name);
-    res.send('ok!');
+    axios.post(slackUrl, { text: 'yeet' })
+        .then(() => res.send('Ok!'))
+        .catch(err => {
+            console.log(err);
+            res.status(500).send('Failed!')
+        });
 })
 
 module.exports = app
